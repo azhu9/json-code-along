c20eba0630ddc1a8ced6a7dcb28c9fd09de50623
azhu9 <109678994+azhu9@users.noreply.github.com>
2022-08-04 15:57:13 -0400

    code along

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /** @TODO - Access the pokemon data from data.js */
 /**         and print to the console to check */
 
-
 /** @TODO - Update the page data using a single pokemon */
 /**  
  *      1. Update the #poke_name heading with the name and dex number
@@ -10,7 +9,28 @@
  *      4. Change the width of each stat bar using the 
  *          pokemon's base stats
  */
+let bulbasaur = pokemon[0];
+console.log(bulbasaur);
 
+let nameHeading = document.querySelector("#poke_name");
+nameHeading.innerHTML = `#${bulbasaur.id} - ${bulbasaur.name}`;
+
+//Changing pokemon picture
+
+let pokeImg = document.querySelector("#poke_img");
+pokeImg.src = bulbasaur.image[1];
+
+//Chaing pokemon type
+let types = document.querySelector("#poke_types");
+types.innerHTML = `${bulbasaur.type[0]}/${bulbasaur.type[1]}`;
+
+//Changing base stats
+let statDivs = document.querySelectorAll("#poke_stats div");
+
+for (let i = 0; i <statDivs.length; i++){
+    let stat = statDivs[i].id;
+    statDivs[i].style['width'] = `${bulbasaur.base[stat] * 4}px`;
+}
 
 /** @TODO - Assign #random_btn to pick a random starter on click */
 /**         and update the page data accordingly
